83b22e29a341f905a6a7475e93ea913dd5f8770b
pvrnh <teaypvrn@gmail.com>
2018-01-16 15:18:25 +0700

    Renew 2!!!!

---
+++ js/gototop.js
@@ -0,0 +1,26 @@
+// Go to top
+window.onscroll = function() {scrollFunction()};
+      function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+              document.getElementById("myBtn").style.display = "block";}
+        else {
+              document.getElementById("myBtn").style.display = "none";}
+      }
+
+      // When the user clicks on the button, scroll to the top of the document
+      function topFunction() {
+          document.body.scrollTop = 0; // For Safari
+          document.documentElement.scrollTop = 0;} // For Chrome, Firefox, IE and Opera
+
+//Filter
+function toggleAndChangeText() {
+     $('#divToToggle').toggle();
+     if ($('#divToToggle').css('display') == 'none') {
+          $('#aTag').html('<i class="fa fa-angle-down" aria-hidden="true"></i> ทุนกู้ยืม');
+     }
+     else {
+          $('#aTag').html('<i class="fa fa-angle-up" aria-hidden="true"></i>&nbsp;ทุนกู้ยืม');
+     }
+}
+
+//For youtub
